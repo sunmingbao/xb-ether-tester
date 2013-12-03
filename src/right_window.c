559fb610ca9890b91ea167b8c6a96321945c4121
sunmingbao <sunmingbao@126.com>
2013-12-03 22:20:55 +0800

    v2.4.2

--- src/right_window.c
+++ src/right_window.c
@@ -13,7 +13,7 @@
 #include "res.h"
 #include "net.h"
 
-const char version[4]={'2','4','1','0'};
+const char version[4]={'2','4','2','0'};
 
 TCHAR szRightWinClassName[] = TEXT ("right_win") ;
 HWND    hwnd_right;
